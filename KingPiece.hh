c1b763c3178389c86317a3eec6363d64185f4d47
Junze Li <li3453@eceprog3.ecn.purdue.edu>
2023-10-30 15:24:30 -0400

    Finished!

--- KingPiece.hh
+++ KingPiece.hh
@@ -1,27 +1,27 @@
-// /*KingPiece.hh*/
-// #ifndef __KINGPIECE_H__
-// #define __KINGPIECE_H__
+/*KingPiece.hh*/
+#ifndef __KINGPIECE_H__
+#define __KINGPIECE_H__
 
-// #include "ChessPiece.hh"
+#include "ChessPiece.hh"
 
-// /**
-//  * Student implementation of a King chess piece.
-//  * The class is defined within the Student namespace.
-//  */
+/**
+ * Student implementation of a King chess piece.
+ * The class is defined within the Student namespace.
+ */
 
-// class ChessPiece;
-// class ChessBoard;
+class ChessPiece;
+class ChessBoard;
 
-// namespace Student
-// {
-//     class KingPiece : public ChessPiece
-//     {
-//         public:
-//             KingPiece(ChessBoard &board, Color color, int row, int column) 
-//                 : ChessPiece(board, color, row, column) {}
+namespace Student
+{
+    class KingPiece : public ChessPiece
+    {
+        public:
+            KingPiece(ChessBoard &board, Color color, int row, int column) 
+                : ChessPiece(board, color, row, column) {}
 
-//     };
-// }
+    };
+}
 
-// #endif
+#endif
 
